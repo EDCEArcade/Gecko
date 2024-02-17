60cba3db04146d1069d944e0cf37d6bded7041ea
EDCEArcade <144564382+EDCEArcade@users.noreply.github.com>
2024-02-17 12:57:13 -0700

    Update calculator.py

--- packages/calculator.py
+++ packages/calculator.py
@@ -1,2 +1,19 @@
-close = lambda: os.system("cls")
-print("hello")
+import os
+clear = lambda: os.system("cls")
+def main():
+	clear()
+	print("Please enter digit #1")
+	d1 = int(input())
+	print("Please enter digit #2")
+	d2 = int(input())
+	print("Please enter operator a, s, d, m")
+	op = input()
+	if op == "a":
+		result = d1 + d2
+	if op == "s":
+		result = d1 - d2
+	if op == "d":
+		result = d1 / d2
+	if op == "m":
+		result = d1 * d2
+	print("The answer is " + result)
